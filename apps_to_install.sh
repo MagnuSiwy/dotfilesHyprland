a2346986a16633e04236b6e735c32ece1437a74e
MagnuSiwy <84531183+MagnuSiwy@users.noreply.github.com>
2023-12-13 22:51:25 +0100

    Add files via upload

---
+++ apps_to_install.sh
@@ -0,0 +1,6 @@
+# sudo pacman -S hyprland firefox vim viewnior vlc kitty mako wireplumber xdg-desktop-portal-hyprland polkit-kde-agent pavucontrol nm-connection-editor bluez bluez-utils blueman dolphin ark swayidle waybar qt5ct qt5-wayland qt6-wayland otf-font-awesome noto-fonts-emoji kvantum wofi ttf-roboto
+
+# yay -S swaylock-effects-git tela-icon-theme nwg-look 
+
+# Podczas instalacji: nano os-prober xdg-user-dirs xdg-user-dirs-gtk nvidia(i wszystko do niej) base-devel ntfs-3g linux-zen-headers linux-lts-headers
+# Po: xdg-user-dirs-update
