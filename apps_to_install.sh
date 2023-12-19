a4172df145ddb14de67f5e9917a83d3914048200
MagnuSiwy <pankacper890@gmail.com>
2023-12-19 22:23:50 +0100

    changed before reinstall

--- apps_to_install.sh
+++ apps_to_install.sh
@@ -1,6 +1,12 @@
+#git clone https://aur.archlinux.org/yay-git.git
+#cd yay-git
+#makepkg -si
+
+#cd ~
+
 # sudo pacman -S hyprland firefox vim viewnior vlc kitty mako wireplumber xdg-desktop-portal-hyprland polkit-kde-agent pavucontrol nm-connection-editor bluez bluez-utils blueman dolphin ark swayidle waybar hyprpaper qt5ct qt5-wayland qt6-wayland otf-font-awesome noto-fonts-emoji kvantum wofi ttf-roboto
 
-# yay -S swaylock-effects-git tela-icon-theme nwg-look 
+# yay -S swaylock-effects-git tela-icon-theme nwg-look bibata-cursor-theme
 
 # Podczas instalacji: nano os-prober xdg-user-dirs xdg-user-dirs-gtk nvidia(i wszystko do niej) base-devel ntfs-3g linux-zen-headers linux-lts-headers
 # Po: xdg-user-dirs-update
