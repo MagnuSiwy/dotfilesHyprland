3b27af02a42c94c6db6725e3245fd575ad977592
MagnuSiwy <pankacper890@gmail.com>
2024-05-23 19:54:22 +0200

    Concept changed

---
+++ scripts/apps_to_install.sh
@@ -0,0 +1,3 @@
+sudo pacman -S --needed pacman-contrib grim slurp wl-clipboard firefox viewnior vlc kitty mako wireplumber xdg-desktop-portal-hyprland polkit-gnome pavucontrol nm-connection-editor ranger waybar hyprpaper qt6ct qt5-wayland qt6-wayland otf-font-awesome noto-fonts-emoji ttf-roboto
+
+yay -S bluez-git blueman-git nwg-look bibata-cursor-theme rofi-wayland 7-zip
