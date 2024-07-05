6caa78697c263ebc8c12fd0a3ae4cade025c0f55
MagnuSiwy <pankacper890@gmail.com>
2024-07-05 08:13:39 +0200

    Added script for a seperate screen only - wlr-randr

--- scripts/apps_to_install.sh
+++ scripts/apps_to_install.sh
@@ -1,3 +1,3 @@
-sudo pacman -S --needed pacman-contrib grim slurp wl-clipboard firefox viewnior vlc kitty mako wireplumber xdg-desktop-portal-hyprland polkit-gnome pavucontrol nm-connection-editor ranger waybar hyprpaper qt6ct qt5-wayland qt6-wayland otf-font-awesome noto-fonts-emoji ttf-roboto
+sudo pacman -S --needed pacman-contrib grim slurp wl-clipboard firefox viewnior vlc kitty mako wireplumber xdg-desktop-portal-hyprland polkit-gnome pavucontrol nm-connection-editor ranger waybar hyprpaper qt6ct qt5-wayland qt6-wayland otf-font-awesome noto-fonts-emoji ttf-roboto wlr-randr
 
 yay -S bluez-git blueman-git nwg-look bibata-cursor-theme rofi-wayland 7-zip
