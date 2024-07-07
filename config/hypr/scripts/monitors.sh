d960080dfae428c4039a27886cd7ea4ed47fdc6c
MagnuSiwy <pankacper890@gmail.com>
2024-07-07 18:46:00 +0200

    wlr-randr changed to hyprctl keyword monitor

--- config/hypr/scripts/monitors.sh
+++ config/hypr/scripts/monitors.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
 if [ $(hyprctl monitors | grep Monitor | wc -l) -ge 2 ]; then
-	hyprctl dispatch swapactiveworkspaces eDP-1 HDMI-A-1
-	wlr-randr --output eDP-1 --off
-#	hyprctl keyword monitor "eDP-1, disable"
-#	hyprctl keyword monitor "HDMI-A-1, 1920x1080@144, 0x0, 1"
+	hyprctl keyword monitor eDP-1,disable
+	hyprctl keyword monitor HDMI-A-1,1920x1080@144,0x0,1
 else
-	hyprctl dispatch swapactiveworkspaces eDP-1 HDMI-A-1
-	wlr-randr --output eDP-1 --on
-#	hyprctl keyword monitor "eDP-1, preferred, auto, 1"
-#	hyprctl keyword monitor "HDMI-A-1, disable"
+	hyprctl keyword monitor eDP-1,1920x1080@120,0x0,1
+	hyprctl keyword monitor HDMI-A-1,disable
 fi
+
+hyprctl dispatch workspace 1
