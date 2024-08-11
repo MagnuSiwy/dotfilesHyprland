d9162b318dde07b3f4ed9bfa089cfc2c3008e98e
MagnuSiwy <pankacper890@gmail.com>
2024-08-11 14:29:55 +0200

    Changed wallpaper, added hyprlock/hypridle, deleted unnecessary scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 # Install basic apps needed for the config to work as intended
 
-yay -S --needed hyprland pacman-contrib grim slurp wl-clipboard firefox viewnior vlc kitty mako xdg-desktop-portal-hyprland polkit-gnome pavucontrol nm-connection-editor ranger waybar hyprpaper qt6ct qt5-wayland qt6-wayland otf-font-awesome noto-fonts-emoji ttf-roboto nwg-look bluez p7zip blueman-git bibata-cursor-theme-bin rofi-wayland everforest-gtk-theme-git tela-icon-theme-bin
+yay -S --needed hyprland hypridle hyprlock pacman-contrib grim slurp wl-clipboard firefox viewnior vlc kitty mako xdg-desktop-portal-hyprland polkit-gnome pavucontrol nm-connection-editor ranger waybar hyprpaper qt6ct qt5-wayland qt6-wayland otf-font-awesome noto-fonts-emoji ttf-roboto nwg-look bluez p7zip blueman-git bibata-cursor-theme-bin rofi-wayland everforest-gtk-theme-git tela-icon-theme-bin
 
 
 # Link the config files to the .config folder
