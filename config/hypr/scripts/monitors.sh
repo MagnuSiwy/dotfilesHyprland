2f9ce711cf6f98a185997dada173ecfb9944b320
MagnuSiwy <pankacper890@gmail.com>
2025-01-07 12:10:25 +0100

    Comment in monitors script now fits the script

--- config/hypr/scripts/monitors.sh
+++ config/hypr/scripts/monitors.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 # This script turns off the laptop screen if an external display is connected
-# You can change the monitor names to whatever you want (hyprctl monitors to list the available monitors)
 
 laptopScreen=$(hyprctl monitors all | grep Monitor | awk '{print $2}' | head -n 1)
 extScreen=$(hyprctl monitors all | grep Monitor | awk '{print $2}' | tail -n 1)
