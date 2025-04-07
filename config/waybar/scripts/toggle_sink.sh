9002aba333a8edbb5a0cf19f22ebdd25ff7b9c6e
MagnuSiwy <pankacper890@gmail.com>
2025-04-07 12:23:17 +0200

    Fix the script failing to set the correct sink

--- config/waybar/scripts/toggle_sink.sh
+++ config/waybar/scripts/toggle_sink.sh
@@ -5,18 +5,21 @@ AVAILABLE_SINKS=$(wpctl status | awk '/Sinks:/ {found=1;next} /Sources:/ {found=
 
 
 for sink in $AVAILABLE_SINKS; do
-    if [[ $sink -ge $DEFAULT_SINK ]]; then
+    echo "Checking $sink"
+    if [ $sink -gt $DEFAULT_SINK ]; then
+        # echo $sink
         wpctl set-default $sink
+    else
+        continue
     fi
 
-    if [[ $DEFAULT_SINK -ne $(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk 'NR==1 {print $2}' | cut -c 1-2) ]]; then
-        echo $sink
+    if [ $sink -eq $(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk 'NR==1 {print $2}' | cut -c 1-2) ]; then
+        # echo $sink
         exit 0
     fi
 done
 
-if [[ $DEFAULT_SINK -eq $(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk 'NR==1 {print $2}' | cut -c 1-2) ]]; then
-    FIRST_SINK=$(echo "$AVAILABLE_SINKS" | head -n 1)
-    echo $FIRST_SINK
-    wpctl set-default $FIRST_SINK
+if [ $DEFAULT_SINK -eq $(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk 'NR==1 {print $2}' | cut -c 1-2) ]; then
+    # echo "Falling back to the first sink"
+    wpctl set-default $(echo "$AVAILABLE_SINKS" | head -n 1)
 fi;
