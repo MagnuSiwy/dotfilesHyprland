439f2a6c4e487de9e1fac0e99d13a5e5aa486c0c
MagnuSiwy <pankacper890@gmail.com>
2025-05-07 20:19:54 +0200

    Change the way sink in changed on click on audio waybar widget

--- config/waybar/scripts/toggle_sink.sh
+++ config/waybar/scripts/toggle_sink.sh
@@ -3,23 +3,19 @@
 DEFAULT_SINK=$(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk 'NR==1 {print $2}' | cut -c 1-2)
 AVAILABLE_SINKS=$(wpctl status | awk '/Sinks:/ {found=1;next} /Sources:/ {found=0} found {print $2}' | grep -G '[0-9][0-9]\.' | cut -c 1-2)
 
-
+echo "Start sink script"
 for sink in $AVAILABLE_SINKS; do
     echo "Checking $sink"
+    
     if [ $sink -gt $DEFAULT_SINK ]; then
-        # echo $sink
         wpctl set-default $sink
-    else
-        continue
-    fi
 
-    if [ $sink -eq $(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk 'NR==1 {print $2}' | cut -c 1-2) ]; then
-        # echo $sink
-        exit 0
+        if [ $sink -eq $(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk 'NR==1 {print $2}' | cut -c 1-2) ]; then
+            echo "Using $sink"
+            exit 0 
+        fi
     fi
 done
 
-if [ $DEFAULT_SINK -eq $(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk 'NR==1 {print $2}' | cut -c 1-2) ]; then
-    # echo "Falling back to the first sink"
-    wpctl set-default $(echo "$AVAILABLE_SINKS" | head -n 1)
-fi;
+echo "Using default $(echo "$AVAILABLE_SINKS" | head -n 1)"
+wpctl set-default $(echo "$AVAILABLE_SINKS" | head -n 1)
